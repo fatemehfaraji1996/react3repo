0361c1917db6560fdd4f7a019a38192dfa28c9d6
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-05-21 23:39:15 +0330

    q2 ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todolist from './Components/todolist'
 import Count from './Components/conter'
 
 function App() {
   return (
     <>
     <Count/>
-    
+    <Todolist/>
     </>
   )
  
